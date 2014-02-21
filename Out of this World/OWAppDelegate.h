18cecbcc7ba021a71c06931df9b42a5d44df2100
benjmins <benjmin.ho@gmail.com>
2014-02-21 14:23:01 -0500

    Initial Commit

---
+++ Out of this World/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  Out of this World
+//
+//  Created by Benjamin on 2/21/2014.
+//  Copyright (c) 2014 Benjamin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
